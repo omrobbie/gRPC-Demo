1d48f8c42d6699dda5e1ade42a9b67b31649e1d9
omrobbie <putu.roby@yahoo.com>
2024-04-23 10:19:14 +0700

    Initial Commit

---
+++ gRPC Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gRPC Demo
+//
+//  Created by omrobbie on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
